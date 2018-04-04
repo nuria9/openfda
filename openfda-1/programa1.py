f62d3a505fc6d8834f885fb01e6824616777f6d9
nuria <nuria.nunez1999@gmail.com>
2018-04-04 21:46:30 +0200

    practicas

--- openfda-1/programa1.py
+++ openfda-1/programa1.py
@@ -3,14 +3,13 @@ import json
 
 headers = {'User-Agent': 'http-client'}
 
-conn = http.client.HTTPSConnection("api.github.com")
-conn.request("GET", "/users/Obijuan/repos", None, headers)
+conn = http.client.HTTPSConnection("api.fda.gov")
+conn.request("GET", "/drug/label.json", None, headers)
 r1 = conn.getresponse()
-print(r1.status, r1.reason)
-repos_raw = r1.read().decode("utf-8")
+r2 = r1.read().decode("utf-8")
 conn.close()
 
-repos = json.loads(repos_raw)
+repos = json.loads(r2)
 
 repo = repos[0]
 print("The owner of the first repository is", repo['owner']['login'])
